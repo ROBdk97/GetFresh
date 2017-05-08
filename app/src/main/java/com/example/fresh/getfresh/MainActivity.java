05d6b29a2aae7932bb531261edcf8dedf8fe1285
ROBdk97 <robletsplays1@googlemail.com>
2017-05-08 18:08:18 +0200

    DBTest

--- app/src/main/java/com/example/fresh/getfresh/MainActivity.java
+++ app/src/main/java/com/example/fresh/getfresh/MainActivity.java
@@ -5,18 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.fresh.getfresh.helpjars.Dbzugriff;
+import com.example.fresh.getfresh.helpjars.test.DBHandler;
+import com.example.fresh.getfresh.helpjars.test.Konto;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     //public static String dataPath = "/com.example.fresh.getfresh/data/data";
-    public Dbzugriff db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        db = new Dbzugriff();
+
+        //TEST##############################
+        System.out.println("#####Dir:"+getFilesDir());
+        Konto k1 = new Konto(1,"Hans", 199.9);
+        DBHandler db = new DBHandler(this);
+        db.addKonnto(k1);
+        System.out.println("#############");
+        List<Konto> kontos = db.getAllShops();
+        for(int i=0;i<kontos.size();i++){
+            System.out.println(kontos.get(i).getKonntonummer()+" "+kontos.get(i).getKname()+" "+kontos.get(i).getKontostand());
+        }
+        System.out.println("#############");
+        //Test-ENDE ################
     }
 
     public void onClickKochbuch(View v)    {
@@ -30,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickTest(View v) {
-        //db.holen(1);
         Intent intent = new Intent(getApplicationContext(), Test_main.class);
         startActivity(intent);
     }
---
+++ app/src/main/java/com/example/fresh/getfresh/helpjars/test/DBHandler.java
@@ -0,0 +1,77 @@
+package com.example.fresh.getfresh.helpjars.test;
+
+/**
+ * Created by Robin on 08.05.2017.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "testDB";
+    // Contacts table name
+    private static final String TABLE_KONTO = "test";
+    // Shops Table Columns names
+    private static final String KEY_ID = "kontonummer";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_KON_STAND = "kontostand";
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_KONTO + "("
+        + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+        + KEY_KON_STAND + " REAL" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+// Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_KONTO);
+// Creating tables again
+        onCreate(db);
+    }
+
+
+    public void addKonnto(Konto pKonto) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, pKonto.getKname()); // Shop Name
+        values.put(KEY_KON_STAND, pKonto.getKontostand()); // Shop Phone Number
+// Inserting Row
+        db.insert(TABLE_KONTO, null, values);
+        db.close(); // Closing database connection
+    }
+
+    public List<Konto> getAllShops() {
+        List<Konto> shopList = new ArrayList<>();
+// Select All Query
+        String selectQuery = "SELECT * FROM " + TABLE_KONTO;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+// looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Konto shop = new Konto();
+                shop.setKonntonummer(Integer.parseInt(cursor.getString(0)));
+                shop.setKname(cursor.getString(1));
+                shop.setKontostand(cursor.getDouble(2));
+// Adding contact to list
+                shopList.add(shop);
+            } while (cursor.moveToNext());
+        }
+// return contact list
+        return shopList;
+    }
+}
