0231acafeb13049f212dcff9fbe0d6259a06883f
Nikolausman <kev.gross97@gmail.com>
2017-05-17 21:01:50 +0200

    premium

--- app/src/main/java/com/example/fresh/getfresh/Test_main.java
+++ app/src/main/java/com/example/fresh/getfresh/Test_main.java
@@ -1,13 +1,50 @@
 package com.example.fresh.getfresh;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 public class Test_main extends AppCompatActivity {
 
+
+    public static Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_main);
+    }
+
+    public void dialogevent(View view){
+
+        btn = (Button)findViewById(R.id.dial);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder altdial = new AlertDialog.Builder(Test_main.this);
+                altdial.setMessage("Bitte kaufen sie die Premium-App im Playstore für weitere Funktionen").setCancelable(false)
+                        .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert = altdial.create();
+                alert.setTitle("Premium kaufen");
+                alert.show();
+
+            }
+        });
+
+
     }
 }
