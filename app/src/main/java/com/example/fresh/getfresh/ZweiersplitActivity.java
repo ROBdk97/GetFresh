c61ff0686f82b8e47f25ba17f8b1e72aef4a3cf5
ROBdk97 <robletsplays1@googlemail.com>
2017-05-18 23:56:14 +0200

    2er Splitt

--- app/src/main/java/com/example/fresh/getfresh/ZweiersplitActivity.java
+++ app/src/main/java/com/example/fresh/getfresh/ZweiersplitActivity.java
@@ -39,22 +39,22 @@ public class ZweiersplitActivity extends AppCompatActivity
         setContentView(R.layout.activity_2erspli);
         
 	//Nicht richtige Ausgabe wegen fehlerhaftem XML
-        setb=(TextView)findViewById(R.id.textView68);
-        widb=(TextView)findViewById(R.id.textView68);
-        sett=(TextView)findViewById(R.id.textView68);
-        widt=(TextView)findViewById(R.id.textView68);
-        setbi=(TextView)findViewById(R.id.textView68);
-        widbi=(TextView)findViewById(R.id.textView68);
-        setr=(TextView)findViewById(R.id.textView68);
-        widr=(TextView)findViewById(R.id.textView68);
-        setbe=(TextView)findViewById(R.id.textView68);
-        widbe=(TextView)findViewById(R.id.textView68);
-        setba=(TextView)findViewById(R.id.textView68);
-        widba=(TextView)findViewById(R.id.textView68);
-        sets=(TextView)findViewById(R.id.textView68);
-        wids=(TextView)findViewById(R.id.textView68);
-        setbe2=(TextView)findViewById(R.id.textView68);
-        widbe2=(TextView)findViewById(R.id.textView68);
+        setb=(TextView)findViewById(R.id.textViewSetBauch);
+        widb=(TextView)findViewById(R.id.textViewBrustWieder);
+        //sett=(TextView)findViewById(R.id.);
+        widt=(TextView)findViewById(R.id.textViewTrizepsWieder);
+        setbi=(TextView)findViewById(R.id.textViewBiezpsSet);
+        widbi=(TextView)findViewById(R.id.textViewBizepsWieder);
+        setr=(TextView)findViewById(R.id.textViewRueckenSet);
+        widr=(TextView)findViewById(R.id.textViewRueckenWieder);
+        setbe=(TextView)findViewById(R.id.textViewBeineSet1);
+        widbe=(TextView)findViewById(R.id.textViewBeineWieder1);
+        setba=(TextView)findViewById(R.id.textViewSetBauch);
+        //widba=(TextView)findViewById(R.id.bau);
+        sets=(TextView)findViewById(R.id.textViewSchulternSet);
+        wids=(TextView)findViewById(R.id.textViewSchulternWiede);
+        setbe2=(TextView)findViewById(R.id.textViewBeineSet2);
+        widbe2=(TextView)findViewById(R.id.textViewBeineWieder2);
         
 
         //#####
@@ -77,12 +77,12 @@ public class ZweiersplitActivity extends AppCompatActivity
         setb.setText(db.getTraining(auf,"Brust").getSaetze());
         widb.setText(db.getTraining(auf,"Brust").getWiederholungen());
         if (auf) {
-            sett.setText(db.getTraining(auf, "Trizeps").getSaetze());
+            //sett.setText(db.getTraining(auf, "Trizeps").getSaetze());
             widt.setText(db.getTraining(auf, "Trizeps").getWiederholungen());
             setb.setText(db.getTraining(auf,"Bizeps").getSaetze());
             widb.setText(db.getTraining(auf,"Bizeps").getWiederholungen());
         }else {
-            sett.setText(db.getTraining(auf, "Arme").getSaetze());
+            //sett.setText(db.getTraining(auf, "Arme").getSaetze());
             widt.setText(db.getTraining(auf, "Arme").getWiederholungen());
         }
         setr.setText(db.getTraining(auf,"Ruecken").getSaetze());
@@ -90,7 +90,7 @@ public class ZweiersplitActivity extends AppCompatActivity
         setbe.setText(db.getTraining(auf,"Beine").getSaetze());
         widbe.setText(db.getTraining(auf,"Beine").getWiederholungen());
         setba.setText(db.getTraining(auf,"Bauch").getSaetze());
-        widba.setText(db.getTraining(auf,"Bauch").getWiederholungen());
+        //widba.setText(db.getTraining(auf,"Bauch").getWiederholungen());
         sets.setText(db.getTraining(auf,"Schultern").getSaetze());
         wids.setText(db.getTraining(auf,"Schultern").getWiederholungen());
         setbe2.setText(db.getTraining(auf,"Beine").getSaetze());
